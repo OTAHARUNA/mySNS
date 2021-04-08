4e61f816edcdc3c9d8801b9ad46c7de898c64a84
OTAHARUNA <radwimps.love867@gmail.com>
2021-04-08 22:58:55 +0900

    最新

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -10,7 +10,7 @@
       <div class="">
           <div>
             <div class="">
-              <p>出席済</p>
+              <p>出席済み</p>
             </div>
             <div class="">
             </div>
